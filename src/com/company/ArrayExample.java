1bedefcb6bfb9bb11ae6044966404b134c4db5bb
Hungyuuki <hungyuuki777@gmail.com>
2022-01-24 19:33:22 +0700

    1

---
+++ src/com/company/ArrayExample.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        ArrayExample arrExample = new ArrayExample();
+        Integer[] arr = arrExample.createRandom();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = scanner.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số 5" + x + " là " + arr[x]);
+        } catch (IndexOutOfBoundsException e) {
+            System.err.println("Chỉ số vượt quá giới hạn của mảng");
+        }
+    }
+
+    public Integer[] createRandom() {
+        Random random = new Random();
+        Integer[] array = new Integer[100];
+        for (int i = 0; i < 100; i++) {
+            array[i] = random.nextInt(100);
+            System.out.println(array[i]);
+        }
+        return array;
+    }
+}
